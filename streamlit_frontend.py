ee7389dbe20ca657f54b938da0bb603cc97a2d00
Ayush Kumar <ayumar115@gmail.com>
2026-03-31 14:33:50 +0530

    feat: langgraph chatbot with streaming

--- streamlit_frontend.py
+++ streamlit_frontend.py
@@ -1,32 +1,32 @@
-# import streamlit as st
-# from langgraph_chatbot_backend import chatbot
-# from langchain_core.messages import HumanMessage
+import streamlit as st
+from langgraph_chatbot_backend import chatbot
+from langchain_core.messages import HumanMessage
 
-# # st.session_state -> dict
-# #defining config for the chatbot, we can add more configurations as needed
-# CONFIG = {'configurable': {'thread_id': 'thread-1'}}
+# st.session_state -> dict
+#defining config for the chatbot, we can add more configurations as needed
+CONFIG = {'configurable': {'thread_id': 'thread-1'}}
 
-# if 'message_history' not in st.session_state:
-#     st.session_state['message_history'] = []
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
 
 
-# #loading the conversation history
-# for message in st.session_state['message_history']:
-#     with st.chat_message(message["role"]):
-#         st.text(message["content"])
+#loading the conversation history
+for message in st.session_state['message_history']:
+    with st.chat_message(message["role"]):
+        st.text(message["content"])
 
-# user_input =st.chat_input("Type here...")
+user_input =st.chat_input("Type here...")
 
-# if user_input:
+if user_input:
 
-#     #first add the message to message_history
-#     st.session_state['message_history'].append({"role":"user", "content": user_input})
-#     with st.chat_message("user"):
-#         st.text(user_input)
+    #first add the message to message_history
+    st.session_state['message_history'].append({"role":"user", "content": user_input})
+    with st.chat_message("user"):
+        st.text(user_input)
 
-#     chatbot_response = chatbot.invoke({'messages':[HumanMessage(content=user_input)]}, config=CONFIG)
-#     ai_message = chatbot_response['messages'][-1].content
+    chatbot_response = chatbot.invoke({'messages':[HumanMessage(content=user_input)]}, config=CONFIG)
+    ai_message = chatbot_response['messages'][-1].content
 
-#     st.session_state['message_history'].append({"role":"assistant", "content": ai_message})
-#     with st.chat_message("assistant"):
-#         st.text(ai_message) 
+    st.session_state['message_history'].append({"role":"assistant", "content": ai_message})
+    with st.chat_message("assistant"):
+        st.text(ai_message) 
