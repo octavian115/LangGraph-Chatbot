df09c4b7463cdac2a3b081877c26cf8c0c6c8799
Ayush Kumar <ayumar115@gmail.com>
2026-04-02 15:10:55 +0530

    switch to Gemini 2.5 flash

--- langgraph_database_backend.py
+++ langgraph_database_backend.py
@@ -2,6 +2,7 @@ from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
 from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langgraph.checkpoint.sqlite import SqliteSaver
 from langgraph.checkpoint.postgres import PostgresSaver
 from langgraph.graph.message import add_messages
@@ -15,7 +16,8 @@ load_dotenv()
 
 DB_URI = os.environ.get("DATABASE_URL")
 
-llm = ChatOpenAI(model="gpt-4o-mini")
+# llm = ChatOpenAI(model="gpt-4o-mini")
+llm = ChatGoogleGenerativeAI(model = "gemini-2.5-flash")
 
 class ChatState(TypedDict):
     messages: Annotated[list[BaseMessage], add_messages]
